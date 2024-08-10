cc3fa06667e7634148361da34ef7678fb6046c18
Karina Arshakuni <arshakunii@icloud.com>
2024-08-09 18:11:16 -0700

    building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,18 +4,19 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 
-interface Games {
+export interface Game {
     id: number;
     name: string;
+    background_image: string
   }
   
 interface FetchGameResponse {
     count: number;
-    results: Games[];
+    results: Game[];
   }
 
 const useGames = () =>{
-      const [games, setGames] = useState<Games[]>([]);
+      const [games, setGames] = useState<Game[]>([]);
       const [error, setError] = useState("");
     
       useEffect(() => {
